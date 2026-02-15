66d2ee0d3efdd7cbab1249f7b71a967354e8f7e3
Sivdvj <151998553+Sivdvj@users.noreply.github.com>
2026-02-15 15:53:48 +0530

    chore: improve ui for sharing

--- src/lib/FriendComp.svelte
+++ src/lib/FriendComp.svelte
@@ -1,19 +1,25 @@
 <script>
-	export let color = "#1c1c1e";
+	export let color = "#000000";
 	export let text = "";
 	export let image = "";
 	export let name = "";
 </script>
 
-<div class="relative mb-4 flex h-48 w-full flex-col justify-between overflow-hidden rounded-4xl bg-gray-300">
-	<img src={image} alt="gradient" class="absolute z-0 h-full w-full shrink-0" />
-	<div class="relative z-1 flex w-full flex-row items-start justify-between p-4">
+<div class="relative mb-4 flex h-48 w-full flex-col justify-between overflow-hidden rounded-3xl bg-[#1c1c1e]">
+	{#if image}
+		<img src={image} alt="gradient" class="absolute z-0 h-full w-full shrink-0" />
+	{/if}
+	<div class="relative flex w-full flex-row-reverse justify-between p-4">
 		<!-- TODO: Notify friend -->
-		<button class="cursor-pointer rounded-4xl bg-white px-3 py-1 hover:bg-gray-500/50">Nudge</button>
+		<button class="rounded-4xl bg-gray-700/50 px-3 py-1 text-white hover:bg-gray-500/50">More</button>
 	</div>
 	<div class="z-10 p-4 text-xl" style="color:{color}">
 		<p class="italic">{name}</p>
-		<p class="font-bold">is feeling {text}</p>
+		{#if text}
+			<p class="font-bold">is feeling {text}</p>
+		{:else}
+			<p class="font-bold">no emotions shared</p>
+		{/if}
 	</div>
 </div>
 
--- src/lib/FriendScreen.svelte
+++ src/lib/FriendScreen.svelte
@@ -9,12 +9,24 @@
 		let res = await request("/friend/getAll");
 		friends = res.list;
 	});
+
+	function getImage(color) {
+		const hex = color.replace("#", "");
+		const r = parseInt(hex.substring(0, 2), 16);
+		const g = parseInt(hex.substring(2, 4), 16);
+		const b = parseInt(hex.substring(4, 6), 16);
+
+		if (b > r && b > g * 0.8) return "/assets/3.png"; // Blue
+		if (g > r && g > b) return "/assets/2.png"; // Green
+		if (g > r * 0.75) return "/assets/4.png"; // Yellow
+		return "/assets/1.png"; // Red
+	}
 </script>
 
 <main>
 	<div class="flex flex-col gap-4 p-4">
 		{#each friends as friend}
-			<FriendComp name={friend.name} />
+			<FriendComp name={friend.name} text={friend.currentEmotion?.text || ""} color={friend.currentEmotion?.color || "#ffffff"} image={friend.currentEmotion ? getImage(friend.currentEmotion.color) : ""} />
 		{/each}
 	</div>
 </main>
